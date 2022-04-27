8cb6d19b43ba2b4b098cf40de2bd2e5f0a875702
D-on3 <dnedyalkov396@gmail.com>
2022-04-27 03:33:04 +0300

    init

---
+++ Conditional Statements - Lab/01. Excellent Result.js
@@ -0,0 +1,11 @@
+
+
+function isExelent(input) {
+
+    let check = Number(input[0]);
+    if(check >= 5.50) {
+        console.log('Excellent!');
+    }
+}
+
+isExelent('4.5')
---
+++ Conditional Statements - Lab/02. Greater Number.js
@@ -0,0 +1,15 @@
+
+
+
+function greatNum(input) {
+    let num1 = Number(input[0]);
+    let num2 = Number(input[1]);
+    if (num1 > num2) {
+        console.log(num1)
+    }
+    else {
+        console.log(num2)
+    }
+}
+
+greatNum(['2','4'])
